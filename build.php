78aace37939a2a1a75ee664e1f9792761b7e2b05
ccq18 <348578429@qq.com>
2018-06-16 02:10:52 +0800

    更新格式

--- build.php
+++ build.php
@@ -1,28 +1,30 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-
+$conf = require __DIR__.'/config.php';
 $s = file_get_contents('./SOURCE_README.md');
-//$rs = FormatMd($s);
 $arrs = explode("\n", $s);
 $http = new \Util\Http();
 //$i=0;
 foreach ($arrs as $k => $line) {
     $ok = preg_match('/\s*\*\s+\[(.*?)\]\((.*?)\)(.*)/', $line, $match);
     if ($ok) {
-
         $rep = del_prefix($match[2], 'https://github.com/');
-
-        $repInfo = $http->getJson('https://api.github.com/search/repositories', ['q' => $rep]);
-        $star = $repInfo['items']['0']['stargazers_count'] ?? '';
+        //取得github star数据
+        $repInfo = $http->getJson("https://{$conf['user']}:{$conf['pass']}@api.github.com/repos/" . $rep);
+        $star = $repInfo['stargazers_count'] ?? '';
         if (!empty($star)) {
             $star = "star:{$star}";
         }
-        $txt = str_translate_diver_google($match[3]);
-        $arrs[$k] = " * [{$match[1]}]({$match[2]}) {$star} $txt ({$match[3]})";
-
-        echo $arrs[$k].PHP_EOL;
-
+        $enTxt = ltrim($match[3], '- ');
+        $txt = '';
+        if (!empty($enTxt)) {
+            $txt = str_translate_diver_google($enTxt);
+            $txt = "** {$txt} **";
+            $enTxt = "({$enTxt})";
+        }
+        $arrs[$k] = " * [{$match[1]}]({$match[2]}) {$star} $txt $enTxt";
+        echo $arrs[$k] . PHP_EOL;
     }
 }
-$s = implode("\n",$arrs);
-file_put_contents('./README.md',$s);
+$s = implode("\n", $arrs);
+file_put_contents('./README.md', $s);
